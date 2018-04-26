69ae2be0ae643c1bbba9d2a4a9ab2dd528b6bf5b
Diego Pergolini <diego.pergolini@studio.unibo.it>
2018-04-26 11:46:58 +0200

    Added main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao Travis");
+        System.out.println("Ciao Travis Armando");
     }
 }
